42e4524df91ea4b027a2d916dba78dc5da1ab372
Bernhard Kau <bernhard.kau@vcat.de>
2020-08-24 21:46:16 +0200

    adding fonts and colors

--- functions.php
+++ functions.php
@@ -29,3 +29,39 @@ function uku_drk_theme_enqueue_styles() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'uku_drk_theme_enqueue_styles' );
+
+/**
+ * Dequeue parent theme Google Fonts.
+ */
+function uku_drk_denqueue_scripts() {
+	wp_dequeue_style( 'uku-fonts' );
+}
+add_action( 'wp_enqueue_scripts', 'uku_drk_denqueue_scripts', 11 );
+add_action( 'enqueue_block_editor_assets', 'uku_drk_denqueue_scripts', 11 );
+
+/**
+ * Disable some parent theme functions and hooks.
+ */
+function uku_drk_disable_customize_css() {
+	remove_action( 'wp_head', 'uku_customize_css' );
+}
+add_action( 'init', 'uku_drk_disable_customize_css', 11 );
+
+/**
+ * Disable some parent theme customizer settings.
+ */
+function uku_drk_customize_deregister() {
+	global $wp_customize;
+
+	$wp_customize->remove_setting( 'uku_link_color' );
+	$wp_customize->remove_setting( 'uku_linkhover_color' );
+	$wp_customize->remove_setting( 'uku_footer_bg_color' );
+	$wp_customize->remove_setting( 'uku_footer_text_color' );
+	$wp_customize->remove_setting( 'uku_offcanvas_bg_color' );
+	$wp_customize->remove_setting( 'uku_offcanvas_text_color' );
+	$wp_customize->remove_setting( 'uku_frontsection_bg_color' );
+	$wp_customize->remove_setting( 'uku_subscribe_bg_color' );
+	$wp_customize->remove_setting( 'uku_aboutsection_bg_color' );
+	$wp_customize->remove_setting( 'uku_shopcats_bg_color' );
+}
+add_action( 'customize_register', 'uku_drk_customize_deregister', 11 );
