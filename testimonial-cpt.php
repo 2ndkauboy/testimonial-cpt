9bc94bc469af0d25d1eddfef73f3bb4ee6cf3bfb
Bernhard Kau <bernhard.kau@vcat.de>
2020-11-22 17:08:26 +0100

    Initial commit

---
+++ testimonial-cpt.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       Testimonial Post Type
+ * Plugin URI:        https://github.com/2ndkauboy/testimonial-cpt/
+ * Description:       Register a post type to manage testimonials.
+ * Version:           1.0.0
+ * Requires at least: 5.5
+ * Requires PHP:      7.2
+ * Author:            Bernhard Kau
+ * Author URI:        https://kau-boys.de
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       testimonial-cpt
+ * Domain Path:       /languages
+ */
